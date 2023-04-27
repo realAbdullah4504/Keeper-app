535fa7a80aff456d475fdc700a357eebdb5dbde9
realAbdullah4504E <abdullahjaved4504@gmail.com>
2023-04-27 06:26:39 -0800

    keeper app part 1 video 383

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Note() {
+    return (
+        <div className="note">
+            <h1>hi</h1>
+            <p>hello</p>
+        </div>
+
+
+    );
+}
